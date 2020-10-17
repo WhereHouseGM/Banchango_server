04dd5f571ab8a7e9d341723f772ad942a40a4615
Sangwoo Ra <robbyra@gmail.com>
2020-10-17 15:04:28 +0900

    warehouse attachments domain done.

---
+++ src/main/java/com/banchango/domain/warehouseattachments/WarehouseAttachments.java
@@ -0,0 +1,23 @@
+package com.banchango.domain.warehouseattachments;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "warehouse_attachments")
+public class WarehouseAttachments {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String url;
+
+    @Column(name = "warehouseId")
+    private Integer warehouseId;
+}
---
+++ src/main/java/com/banchango/domain/warehouseattachments/WarehouseAttachmentsRepository.java
@@ -0,0 +1,6 @@
+package com.banchango.domain.warehouseattachments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WarehouseAttachmentsRepository extends JpaRepository<WarehouseAttachments, Integer> {
+}
