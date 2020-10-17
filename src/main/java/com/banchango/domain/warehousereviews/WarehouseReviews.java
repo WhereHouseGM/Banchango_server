4c648d426b22da6bce34e735bb48e9bafb9695e8
Sangwoo Ra <robbyra@gmail.com>
2020-10-17 15:07:26 +0900

    warehouse reviews domain done.

---
+++ src/main/java/com/banchango/domain/warehousereviews/WarehouseReviews.java
@@ -0,0 +1,29 @@
+package com.banchango.domain.warehousereviews;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "warehouse_reviews")
+public class WarehouseReviews {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @Column(name = "writerId")
+    private Integer writerId;
+
+    @Column(name = "warehouseId")
+    private Integer warehouseId;
+}
---
+++ src/main/java/com/banchango/domain/warehousereviews/WarehouseReviewsRepository.java
@@ -0,0 +1,6 @@
+package com.banchango.domain.warehousereviews;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WarehouseReviewsRepository extends JpaRepository<WarehouseReviews, Integer> {
+}
