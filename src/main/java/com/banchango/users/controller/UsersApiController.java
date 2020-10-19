d41a27dde2862bc918894e2679a47995a363e2eb
Sangwoo Ra <robbyra@gmail.com>
2020-10-19 18:59:44 +0900

    Remove unused import.

--- src/main/java/com/banchango/users/controller/UsersApiController.java
+++ src/main/java/com/banchango/users/controller/UsersApiController.java
@@ -11,7 +11,6 @@ import com.banchango.users.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
