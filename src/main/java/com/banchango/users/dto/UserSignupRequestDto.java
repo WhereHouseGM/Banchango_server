98b9c1dcaa9a690af9033af7411d6a60c1f1ed4d
Sangwoo Ra <robbyra@gmail.com>
2020-10-20 12:47:45 +0900

    Remove unused method convertMap().

--- src/main/java/com/banchango/users/dto/UserSignupRequestDto.java
+++ src/main/java/com/banchango/users/dto/UserSignupRequestDto.java
@@ -4,8 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.HashMap;
-
 @NoArgsConstructor
 @Getter
 @Setter
@@ -18,16 +16,4 @@ public class UserSignupRequestDto {
     private String telephoneNumber;
     private String companyName;
     private String phoneNumber;
-
-    public HashMap<String, Object> convertMap(int id) {
-        HashMap<String, Object> map = new HashMap<>();
-        map.put("id", id);
-        map.put("name", name);
-        map.put("email", email);
-        map.put("type", type);
-        map.put("telephoneNumber", telephoneNumber);
-        map.put("phoneNumber", phoneNumber);
-        map.put("companyName", companyName);
-        return map;
-    }
 }
