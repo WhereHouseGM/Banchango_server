7c7b50b002e937ec9e52ae64a91a12776748fd95
nostalgia <skfk0135@gmail.com>
2020-11-18 19:10:17 +0900

    kill java before restart

--- scripts/run-application.sh
+++ scripts/run-application.sh
@@ -1,2 +1,3 @@
+sudo pkill -6 java
 source /home/ubuntu/.env
 SPRING_PROFILES_ACTIVE=production nohup java -jar /home/ubuntu/banchango/build/libs/*.jar 1>>/home/ubuntu/log/spring-log.log 2>>/home/ubuntu/log/spring-error.log &
