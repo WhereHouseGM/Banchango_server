6b92970a8a2ca57f7d5f678c80d76635ed40ed6f
sangwoo-98 <robbyra@gmail.com>
2020-11-20 18:10:00 +0900

    Create Domain classes for warehouse main images.

---
+++ src/main/java/com/banchango/domain/warehousemainimages/WarehouseMainImagesRepository.java
@@ -0,0 +1,7 @@
+package com.banchango.domain.warehousemainimages;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WarehouseMainImagesRepository extends JpaRepository<WarehouseMainImages, Integer> {
+
+}
