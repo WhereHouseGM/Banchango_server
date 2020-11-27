af18262d46028273eb220eacd09754a1b7005da5
sangwoo-98 <robbyra@gmail.com>
2020-11-27 13:13:00 +0900

    Revert "Revert "Added domain classes for warehouse facility usages, warehouse usage cautions.""
    This reverts commit c8d0d137a9ecebe5b58ff0245a7e58a993b1b3b4.

---
+++ src/main/java/com/banchango/domain/warehouseusagecautions/WarehouseUsageCautions.java
@@ -0,0 +1,30 @@
+package com.banchango.domain.warehouseusagecautions;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "warehouse_usage_cautions")
+public class WarehouseUsageCautions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer warehouseId;
+
+    @Builder
+    public WarehouseUsageCautions(String content, Integer warehouseId) {
+        this.content = content;
+        this.warehouseId = warehouseId;
+    }
+}
