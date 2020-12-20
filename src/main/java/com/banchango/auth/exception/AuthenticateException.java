87b9dd7b41196451249708a6935fbeb47ea950e2
nostalgia <skfk0135@gmail.com>
2020-12-20 19:12:58 +0900

    refactor: use responsestatus annotation for error handling

--- src/main/java/com/banchango/auth/exception/AuthenticateException.java
+++ src/main/java/com/banchango/auth/exception/AuthenticateException.java
@@ -1,5 +1,9 @@
 package com.banchango.auth.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class AuthenticateException extends RuntimeException{
 
     private static final long serialVersionUID = 1L;
