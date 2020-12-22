b902c6b491f7fbc1a29f164d02aae2d6ff78bde7
nostalgia <skfk0135@gmail.com>
2020-12-22 20:48:50 +0900

    fix

--- src/main/java/com/banchango/common/dto/BasicMessageResponseDto.java
+++ src/main/java/com/banchango/common/dto/BasicMessageResponseDto.java
@@ -1,10 +1,12 @@
 package com.banchango.common.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class BasicMessageResponseDto {
     private String message;
 }
