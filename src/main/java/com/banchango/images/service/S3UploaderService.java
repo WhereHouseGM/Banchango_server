0e8009232f792633bf965b2b7b8540a3e84f891d
나상우 <robbyra@gmail.com>
2021-01-01 18:22:00 +0900

    Cleanup codes, both APIs for uploading images are verified.

--- src/main/java/com/banchango/images/service/S3UploaderService.java
+++ src/main/java/com/banchango/images/service/S3UploaderService.java
@@ -1,15 +1,5 @@
 package com.banchango.images.service;
 
-//import com.amazonaws.auth.AWSCredentials;
-//import com.amazonaws.auth.AWSStaticCredentialsProvider;
-//import com.amazonaws.auth.BasicAWSCredentials;
-//import com.amazonaws.services.s3.AmazonS3;
-//import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-//import com.amazonaws.services.s3.model.CannedAccessControlList;
-//import com.amazonaws.services.s3.model.DeleteObjectRequest;
-//import com.amazonaws.services.s3.model.ObjectMetadata;
-//import com.amazonaws.services.s3.model.PutObjectRequest;
-//import com.amazonaws.util.IOUtils;
 import com.banchango.auth.token.JwtTokenUtil;
 import com.banchango.common.dto.BasicMessageResponseDto;
 import com.banchango.common.exception.InternalServerErrorException;
@@ -25,24 +15,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.S3Utilities;
 import software.amazon.awssdk.services.s3.model.*;
-import software.amazon.awssdk.utils.IoUtils;
 
 import javax.annotation.PostConstruct;
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -67,40 +47,22 @@ public class S3UploaderService {
 
     @PostConstruct
     public void setS3Client() {
-        AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-
-        s3Client = S3Client.builder().credentialsProvider(() -> AwsBasicCredentials.create(accessKey, secretKey))
+        s3Client = S3Client.builder()
+                .credentialsProvider(() -> AwsBasicCredentials.create(accessKey, secretKey))
                 .region(Region.AP_NORTHEAST_2).build();
-//        s3Client = AmazonS3ClientBuilder.standard()
-//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-//                .withRegion(this.region)
-//                .build();
     }
 
     private String uploadFile(MultipartFile file) {
         try {
-            //software.amazon.awssdk.services.s3.model.
-            //ObjectMetadata objectMetadata = new ObjectMetadata();
-            //file.getBytes()
-            byte[] bytes = IoUtils.toByteArray(file.getInputStream());
-            //objectMetadata.setContentLength(bytes.length);
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
 
-            String fileName = file.getOriginalFilename();
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(bucket)
                     .contentLength(file.getSize())
                     .key(file.getOriginalFilename())
                     .acl(ObjectCannedACL.PUBLIC_READ)
                     .build();
-            PutObjectResponse response = s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
-            //PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, fileName, byteArrayInputStream);
-            //s3Client.putObject(putObjectRequest.withCannedAcl(CannedAccessControlList.PublicRead));
-            //return s3Client.getUrl(bucket, fileName).toString();
-            GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                    .bucket(bucket)
-                    .key(file.getOriginalFilename())
-                    .build();
+
+            s3Client.putObject(putObjectRequest, RequestBody.fromBytes(file.getBytes()));
 
             S3Utilities s3Utilities = s3Client.utilities();
             GetUrlRequest getUrlRequest = GetUrlRequest.builder()
@@ -108,10 +70,6 @@ public class S3UploaderService {
                     .key(file.getOriginalFilename())
                     .build();
             return s3Utilities.getUrl(getUrlRequest).toString();
-            //String url = s3Client.getObject(getObjectRequest).response().toString();
-            //System.out.println(url);
-           // return null;
-            //return s3Client.getObject(getObjectRequest);
         } catch(IOException exception) {
             throw new InternalServerErrorException(exception.getMessage());
         }
@@ -143,11 +101,9 @@ public class S3UploaderService {
         if(!isUserAuthenticatedToModifyWarehouseInfo(JwtTokenUtil.extractUserId(token), warehouseId)) {
             throw new WarehouseInvalidAccessException();
         }
-        System.out.println("USER IS VALIDATED");
         if(warehouseImagesRepository.findByWarehouseIdAndIsMain(warehouseId, false).size() >= 5) {
             throw new WarehouseExtraImageLimitException();
         }
-        System.out.println("IMAGE IS UPLOADABLE");
         Warehouses warehouse = warehousesRepository.findById(warehouseId).orElseThrow(WarehouseIdNotFoundException::new);
         String url = uploadFile(file);
         WarehouseImages image = WarehouseImages.builder().url(url).isMain(false).warehouse(warehouse).build();
