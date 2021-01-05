7a1db799df8269208296498e165874545f5141ee
nostalgia <skfk0135@gmail.com>
2021-01-05 17:38:16 +0900

    테스트 작성

--- src/main/java/com/banchango/warehouses/dto/WarehouseSummaryDto.java
+++ src/main/java/com/banchango/warehouses/dto/WarehouseSummaryDto.java
@@ -1,5 +1,10 @@
 package com.banchango.warehouses.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class WarehouseSummaryDto {
     private Integer id;
     private String name;
